912b7d8bff0574fe387036405a6264f6eceaa3f4
Fernando Ferreira <168456690+Lz-Fernando@users.noreply.github.com>
2025-04-05 21:43:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,35 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import uvicorn
 import pickle
-import nltk
-import unicodedata
 import string
+import unicodedata
 import os
 
-# Inicializando a aplicação FastAPI
 app = FastAPI()
 
-# Função para pré-processamento de texto
 def process_text_conteudo(conteudo: str) -> str:
-  
-  # Remove pontuação
-  nopont = ''.join([char for char in conteudo if char not in string.punctuation])
-  
-  # Converte para minúsculas
-  nopont = nopont.lower()
-  
-  # Remove acentos
-  nopont = unicodedata.normalize("NFKD", nopont).encode("ASCII", "ignore").decode("ASCII")
-  
-  return nopont
+    nopont = ''.join([char for char in conteudo if char not in string.punctuation])
+    nopont = nopont.lower()
+    nopont = unicodedata.normalize("NFKD", nopont).encode("ASCII", "ignore").decode("ASCII")
+    return nopont
 
-# Função de tokenização (opcional para vetorizador)
-def tokenize_text(conteudo):
-  return conteudo.split()
-
-# Carregando o modelo treinado e o vetor TF-IDF na inicialização da API
 base_dir = os.path.dirname(__file__)
 modelo = pickle.load(open(os.path.join(base_dir, 'modelo.pk1'), 'rb'))
 vectorizer = pickle.load(open(os.path.join(base_dir, 'tfidf_vectorizer.pk1'), 'rb'))
 
-# Definição do esquema de entrada para a API
+def tokenize_text(conteudo):
+  return conteudo.split()
+
 class Mensagem(BaseModel):
-  descricao: str
+    descricao: str
 
 @app.get("/")
 def read_root():
     return {"mensagem": "API de verificação de fraude online"}
 
-# Endpoint para verificar se uma mensagem é fraude ou não
 @app.post("/verificar")
 def verificar_mensagem(mensagem: Mensagem):
-  # Pré-processa a mensagem recebida
-  texto_processado = process_text_conteudo(mensagem.descricao)
-  
-  # Transforma a mensagem para o formato esperado pelo modelo
-  texto_transformado = vectorizer.transform([texto_processado])
-  
-  # Faz a previsão
-  predicao = modelo.predict(texto_transformado)
-  
-  # Retorna o resultado
-  return {"resultado": "FRAUDE" if predicao[0] == 1 else "OK"}
+    texto_processado = process_text_conteudo(mensagem.descricao)
+    texto_transformado = vectorizer.transform([texto_processado])
+    predicao = modelo.predict(texto_transformado)
+    return {"resultado": "FRAUDE" if predicao[0] == 1 else "OK"}
